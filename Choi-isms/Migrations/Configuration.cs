7d9b26c2fba3d62563efad9eb9e4e145102938e6
Derek Woo <derek.c.woo@gmail.com>
2014-07-22 12:17:48 -0700

    Comment out Seed data.
    Add aspnet:UseHostHeaderForRequestUrl app setting and set to true to avoid port numbers from load balancer appearing in URL for unsubscribe emails.

--- Choi-isms/Migrations/Configuration.cs
+++ Choi-isms/Migrations/Configuration.cs
@@ -17,26 +17,26 @@ namespace Choiisms.Migrations
 
 		protected override void Seed(Choiisms.DAL.ChoiismContext context)
 		{
-			context.Choiisms.AddOrUpdate(
-				c => c.ChoiismValue,
-				new Choiism() { ChoiismValue = "You can never have enough ketchup.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "Check your door to make sure it's closed at least six times.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "Unlock the locked door and lock it again to make sure.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "https://farm3.staticflickr.com/2912/14484666159_0500a86bb0_z.jpg", ChoiismType = "image", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "I gave it to your sister.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "I'm always late. People can wait for me.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "It's ok officer... I'm a 4.0 GPA student.", ChoiismType = "string", Submitter = "Ruben Munoz" },
-				new Choiism() { ChoiismValue = "You can never shoot a basketball off the backboard hard enough.", ChoiismType = "string", Submitter = "Derek Woo" },
-				new Choiism() { ChoiismValue = "You can never have enough ranch.", ChoiismType = "string", Submitter = "Ruben Munoz" }
-				);
+			//context.Choiisms.AddOrUpdate(
+			//	c => c.ChoiismValue,
+			//	new Choiism() { ChoiismValue = "You can never have enough ketchup.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "Check your door to make sure it's closed at least six times.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "Unlock the locked door and lock it again to make sure.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "https://farm3.staticflickr.com/2912/14484666159_0500a86bb0_z.jpg", ChoiismType = "image", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "I gave it to your sister.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "I'm always late. People can wait for me.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "It's ok officer... I'm a 4.0 GPA student.", ChoiismType = "string", Submitter = "Ruben Munoz" },
+			//	new Choiism() { ChoiismValue = "You can never shoot a basketball off the backboard hard enough.", ChoiismType = "string", Submitter = "Derek Woo" },
+			//	new Choiism() { ChoiismValue = "You can never have enough ranch.", ChoiismType = "string", Submitter = "Ruben Munoz" }
+			//	);
 
-			context.Subscribers.AddOrUpdate(
-				s => s.EmailAddress,
-				new Subscriber() { EmailAddress = "derek.c.woo@gmail.com", Name = "Derek Woo", EmailHash = "F109A467CCF574D1E8EEE25949A9CA2BEEE88F61" }
-				);
+			//context.Subscribers.AddOrUpdate(
+			//	s => s.EmailAddress,
+			//	new Subscriber() { EmailAddress = "derek.c.woo@gmail.com", Name = "Derek Woo", EmailHash = "F109A467CCF574D1E8EEE25949A9CA2BEEE88F61" }
+			//	);
 
-			base.Seed(context);
-			context.SaveChanges();
+			//base.Seed(context);
+			//context.SaveChanges();
 		}
 	}
 }
